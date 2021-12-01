533d5f6f952a34e28ff5f9e0d6d5790d3593c63e
PhilPinsdorf <philemon.pinsdorf@live.de>
2021-12-02 00:58:06 +0100

    Deleted Wrong Files

--- 2020/Day_1/part_1.py
+++
@@ -1,7 +0,0 @@
-f = open("./input.txt", "r")
-lines = f.read().splitlines()
-
-result = map(lambda a, b: b > a, lines, lines[1:])
-stripped = list(filter(lambda a: a != False, result))
-
-print(len(stripped) + 1)
