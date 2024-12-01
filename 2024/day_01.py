b22ab22ef533f5b193b50a5671460616b3d0f988
Phil Pinsdorf <62160426+PhilPinsdorf@users.noreply.github.com>
2024-12-01 10:33:19 +0000

    Solve Day 1

--- 2024/day_01.py
+++ 2024/day_01.py
@@ -1,1 +1,22 @@
-Test
+first_numbers = []
+second_numbers = []
+count_1 = 0
+count_2 = 0
+
+f = open("input.txt", "r")
+for line in f:
+  line = line.split()
+  first_numbers.append(int(line[0]))
+  second_numbers.append(int(line[1]))
+
+first_numbers.sort()
+second_numbers.sort()
+
+for num in range(len(first_numbers)):
+  count_1 += abs(first_numbers[num] - second_numbers[num])
+
+for num in first_numbers:
+  count_2 += num * second_numbers.count(num)
+
+print(count_1)
+print(count_2)
