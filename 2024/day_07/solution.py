5c8a3114453c36eb3797c4c893cbd2dcc42619d4
Phil Pinsdorf <philemon.pinsdorf@live.de>
2024-12-07 21:52:40 +0100

    Updated Day 7

--- 2024/day_07/solution.py
+++ 2024/day_07/solution.py
@@ -1,5 +1,3 @@
-import multiprocessing
-
 sequences = []
 count_1, count_2, iter_count = 0, 0, 0
 
@@ -8,65 +6,25 @@ for line in f:
     parts = line.split(':')
     sequences.append((int(parts[0]), list(map(int, parts[1].split()))))
 
-def find_solutions_recursive_1(current, sequence, position, solutions):
+def find_solutions_recursive(current, sequence, position, solutions, combine):
     if(position == len(sequence)):
         solutions.add(current)
         return
 
-    find_solutions_recursive_1(current + sequence[position], sequence, position + 1, solutions)
-    find_solutions_recursive_1(current * sequence[position], sequence, position + 1, solutions)
+    find_solutions_recursive(current + sequence[position], sequence, position + 1, solutions, combine)
+    find_solutions_recursive(current * sequence[position], sequence, position + 1, solutions, combine)
+    if combine: find_solutions_recursive(int(str(current) + str(sequence[position])), sequence, position + 1, solutions, combine)
 
-def find_solutions_recursive_2(current, sequence, position, solutions):
-    if(position == len(sequence)):
-        solutions.add(current)
-        return
-
-    find_solutions_recursive_2(current + ' + ' + str(sequence[position]), sequence, position + 1, solutions)
-    find_solutions_recursive_2(current + ' * ' + str(sequence[position]), sequence, position + 1, solutions)
-    find_solutions_recursive_2(current + ' | ' + str(sequence[position]), sequence, position + 1, solutions)
-
-def interpret_string(string):
-    operations = string.split()
+counter = 0
+for sequence in sequences: 
+    solutions_1, solutions_2, found_1, found_2 = set(), set(), 0, 0
+    find_solutions_recursive(sequence[1][0], sequence[1], 1, solutions_1, False)
+    find_solutions_recursive(sequence[1][0], sequence[1], 1, solutions_2, True)
     
-    while(len(operations) > 1):
-        match operations[1]:
-            case '+':
-                operations[0] = str(int(operations[0]) + int(operations[2]))
-            case '*':
-                operations[0] = str(int(operations[0]) * int(operations[2]))
-            case '|':
-                operations[0] = operations[0] + operations[2]
-        
-        del operations[1]
-        del operations[1]
-        
-    return int(operations[0])
-
-def calculate_result(args):
-    sequence, index = args  
-    solutions_1, solutions_2 = set(), set()
-    found_1, found_2 = 0, 0
-    find_solutions_recursive_1(sequence[1][0], sequence[1], 1, solutions_1)
-    find_solutions_recursive_2(str(sequence[1][0]), sequence[1], 1, solutions_2)
+    if sequence[0] in solutions_1: count_1 += sequence[0]
+    if sequence[0] in solutions_2: count_2 += sequence[0]
     
-    if sequence[0] in solutions_1:
-        found_1 = sequence[0]
-    
-    for solution in solutions_2:
-        result = interpret_string(solution)
-        
-        if result == sequence[0]:
-            found_2 = sequence[0]
-            print(index)
-            return (found_1, found_2)
-    
-    return (found_1, found_2)
-    
-with multiprocessing.Pool() as pool:
-    args = [(sequence, index) for index, sequence in enumerate(sequences)]
-    results = pool.map(calculate_result, args)
-    for found_1, found_2 in results:
-        count_1 += found_1
-        count_2 += found_2
+    counter += 1
+    print(counter)
 
 print(count_1, count_2)
